45d002a17687181e004b4e0a83902d65a526a99f
undertailx <robloxidgmpassstand@gmail.com>
2024-11-16 23:32:29 +0700

    added-acon_time

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 10000,
 });
 
 // เชื่อมต่อกับฐานข้อมูล
